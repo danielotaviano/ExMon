04928b6c82281c35bed5f5052fbe2eb1e4cf4717
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-08 11:52:43 -0300

    fix: gameover error

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -18,11 +18,19 @@ defmodule ExMon do
   end
 
   def make_move(move) do
+    Game.info()
+    |> Map.get(:status)
+    |> handle_status(move)
+
+    computer_move(Game.info())
+  end
+
+  defp handle_status(:game_over, _move), do: Status.print_round_message(Game.info())
+
+  defp handle_status(_other, move) do
     move
     |> Actions.fetch_move()
     |> do_move()
-
-    computer_move(Game.info())
   end
 
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
@@ -37,6 +45,7 @@ defmodule ExMon do
   end
 
   defp computer_move(%{turn: :computer, status: :continue}) do
+    IO.puts("aaaaaaaaaaaa")
     move = {:ok, Enum.random(@computer_moves)}
     do_move(move)
   end
